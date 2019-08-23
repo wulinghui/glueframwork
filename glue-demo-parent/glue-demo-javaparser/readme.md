7d749311f7e638730abb0bbbc783b19a44f74a29
wulinghui <873406903@qq.com>
2019-08-23 18:06:45 +0800

    ''

---
+++ glue-demo-parent/glue-demo-javaparser/readme.md
@@ -0,0 +1,8 @@
+- eclipse jdt的缺点
+对于想直接用
+String str = "String str = null;";
+转化成AST是没有办法的，
+必须一个一个添加，太麻烦了。
+就想它的文档，生成一个helloword都费死劲了。
+- 以为可以找到一个代替eclipse jdt的框架，弥补他的缺点。
+找到了这个框架，但是他也不能String <> AST 互转。
---
+++ glue-parent/glue-commons/src/main/java/com/glueframework/common/util/PathClass.java
@@ -0,0 +1,17 @@
+package com.glueframework.common.util;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PathClass {
+	public static String getClassPath(Class<?> c) {
+		return Thread.currentThread().getContextClassLoader().getResource("").getFile();//	/E:/workSpace/Util5/bin/test/
+	}
+	public static String getWorkPath() throws IOException {
+		return new File("").getCanonicalPath();//	E:\workSpace\Util5
+	}
+	public static String getJarPath() {
+		//E:\workSpace\Util5\bin;F:\xvxm\32\eclipse\plugins\org.junit_4.12.0.v201504281640\junit.jar;F:\xvxm\32\eclipse\plugins\org.hamcrest.core_1.3.0.v201303031735.jar;F:\xvxm\32\eclipse\configuration\org.eclipse.osgi\413\0\.cp;F:\xvxm\32\eclipse\configuration\org.eclipse.osgi\412\0\.cp
+		return System.getProperty("java.class.path");
+	}
+}
