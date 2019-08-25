df995028a8b8daa1d5898665c02654049de65ead
wulinghui <873406903@qq.com>
2019-08-25 12:15:56 +0800

    ''

--- glue-parent/glue-framwork/src/main/java/com/glue/framework/boilerplate/ICompilationUnitHandle.java
+++
@@ -1,7 +0,0 @@
-package com.glue.framework.boilerplate;
-
-import org.eclipse.jdt.core.dom.CompilationUnit;
-
-public interface ICompilationUnitHandle {
-	public void doHandle(CompilationUnit compilationUnit);
-}
--- glue-parent/glue-framwork/src/main/java/com/glue/framework/log/ILogFactory.java
+++
@@ -1,5 +0,0 @@
-package com.glue.framework.log;
-
-public interface ILogFactory {
-	ILogger newLogger();
-}
